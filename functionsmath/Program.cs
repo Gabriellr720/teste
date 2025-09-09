028b992540133a69463d5ce59ade1a4f9d99e43f
YuriAoyagui <yuri.aoyagui111@hotmail.com>
2025-09-09 09:09:06 -0300

    MelhorCalculadoraDoBrasil

---
+++ functionsmath/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("_____Math_____");
+Console.WriteLine("Digite um número");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while(i <= 10)
+{
+    Console.WriteLine(num + "x" + i + "=" + num * i);
+    i++;
+}
